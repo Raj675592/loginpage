2e69d8ccde046dc076fcf298e8c2fe2249aebcc4
Raj675592 <ahirwarrajkumar224@gmail.com>
2025-02-01 17:44:22 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,28 @@
+import logo from './logo.svg';
+import './App.css';
+import { useState } from "react"
+import InputPokemon from "./components/InputPokemon"
+import Card from "./components/card";
+function App() {
+  const [pokemonList,setPokemonList]=useState([]);
+  const [addedIdList,setAddedIdList]=useState([])
+  const adderFunction=function (data){
+    if(!addedIdList.includes(data.id)){
+      setAddedIdList([...addedIdList,data.id]);
+      setPokemonList([...pokemonList,data]);
+    }
+  }
+  const cardList=pokemonList.map((data)=>{
+    return <Card key={data.id} src={data.sprites.front_default} name={data.name}/>
+  })
+  return (
+    <>
+      <InputPokemon adderFunction={adderFunction}/>
+      <div className="pokemonContainer">
+        {cardList}
+      </div>
+    </>
+  );
+}
+
+export default App;
